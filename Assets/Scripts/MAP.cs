3f577ef5f01bc7161ec19664012a64a0b756001d
Nathaniel Nelson <nnelson5302@gmail.com>
2019-03-13 11:56:22 -0700

    Made Map n' stuff

---
+++ Assets/Scripts/MAP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MAP : MonoBehaviour {
+
+    public Image Map;
+    public Button libertyville;
+
+	void Start (){
+    }
+	
+	void Update () 
+    {
+        //Move the map
+        Vector2 tempV = new Vector2(-Input.GetAxis("Horizontal")*2, -Input.GetAxis("Vertical")*2);
+        Map.rectTransform.anchoredPosition += tempV;
+
+        //Zoom the map
+        Map.GetComponent<RectTransform>().offsetMin += new Vector2(-Input.GetAxis("Mouse ScrollWheel")*200, -Input.GetAxis("Mouse ScrollWheel")*200);
+        Map.GetComponent<RectTransform>().offsetMax += new Vector2(Input.GetAxis("Mouse ScrollWheel")*200, Input.GetAxis("Mouse ScrollWheel")*200);
+    }
+}
