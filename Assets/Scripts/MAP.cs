aa4c1348902c51be452af9a4f6cbd0c1141a1892
Nathaniel Nelson <nnelson5302@gmail.com>
2019-03-15 11:25:57 -0700

    I fixed the map then kinda screwed it up again

--- Assets/Scripts/MAP.cs
+++ Assets/Scripts/MAP.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class MAP : MonoBehaviour {
 
     public Image Map;
-    public Button libertyville;
+    public Button Libertyville;
 
 	void Start (){
     }
@@ -14,11 +14,29 @@ public class MAP : MonoBehaviour {
 	void Update () 
     {
         //Move the map
-        Vector2 tempV = new Vector2(-Input.GetAxis("Horizontal")*2, -Input.GetAxis("Vertical")*2);
+        Vector2 tempV = new Vector2(-Input.GetAxis("Horizontal") * 2, -Input.GetAxis("Vertical") * 2);
         Map.rectTransform.anchoredPosition += tempV;
 
         //Zoom the map
-        Map.GetComponent<RectTransform>().offsetMin += new Vector2(-Input.GetAxis("Mouse ScrollWheel")*200, -Input.GetAxis("Mouse ScrollWheel")*200);
-        Map.GetComponent<RectTransform>().offsetMax += new Vector2(Input.GetAxis("Mouse ScrollWheel")*200, Input.GetAxis("Mouse ScrollWheel")*200);
+        if (Map.GetComponent<RectTransform>().anchorMin.x <= 1.5 && Map.GetComponent<RectTransform>().anchorMin.y <= 1.5 &&
+            Map.GetComponent<RectTransform>().anchorMax.x >= 1 && Map.GetComponent<RectTransform>().anchorMax.y >= 1
+            && Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            Map.GetComponent<RectTransform>().anchorMin += new Vector2(-Input.GetAxis("Mouse ScrollWheel"), -Input.GetAxis("Mouse ScrollWheel"));
+            Map.GetComponent<RectTransform>().anchorMax += new Vector2(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"));
+            Libertyville.GetComponent<RectTransform>().anchorMin = Map.GetComponent<RectTransform>().anchorMin;
+            Libertyville.GetComponent<RectTransform>().anchorMax = Map.GetComponent<RectTransform>().anchorMax;
+        }
+
+        else if (Map.GetComponent<RectTransform>().anchorMin.x >= -2.7 && Map.GetComponent<RectTransform>().anchorMin.y >= -2.7 &&
+                Map.GetComponent<RectTransform>().anchorMax.x <= 3.7 && Map.GetComponent<RectTransform>().anchorMax.y <= 3.7 &&
+                Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            Map.GetComponent<RectTransform>().anchorMin += new Vector2(-Input.GetAxis("Mouse ScrollWheel"), -Input.GetAxis("Mouse ScrollWheel"));
+            Map.GetComponent<RectTransform>().anchorMax += new Vector2(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"));
+            Libertyville.GetComponent<RectTransform>().anchorMin = Map.GetComponent<RectTransform>().anchorMin;
+            Libertyville.GetComponent<RectTransform>().anchorMax = Map.GetComponent<RectTransform>().anchorMax
+        }
+        
     }
 }
