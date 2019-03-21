10edccd3a3d8614b5a66bb1e21fd80028d8c27fe
Aidan S-W <aidan.ksw@gmail.com>
2019-03-21 11:50:37 -0700

    Functional zoom on map
    Also added tooltip for city buttons

--- Assets/Scripts/MAP.cs
+++ Assets/Scripts/MAP.cs
@@ -14,18 +14,19 @@ public class MAP : MonoBehaviour {
 	void Update () 
     {
         //Move the map
-        Vector2 tempV = new Vector2(-Input.GetAxis("Horizontal") * 2, -Input.GetAxis("Vertical") * 2);
+        Vector2 tempV = new Vector2(-Input.GetAxis("Horizontal") * 4, -Input.GetAxis("Vertical") * 4);
         Map.rectTransform.anchoredPosition += tempV;
-
-        //Zoom the map
+		//Zoom the map
+		//I hope we never have to resurrect this beast
+		/*
         if (Map.GetComponent<RectTransform>().anchorMin.x <= 1.5 && Map.GetComponent<RectTransform>().anchorMin.y <= 1.5 &&
             Map.GetComponent<RectTransform>().anchorMax.x >= 1 && Map.GetComponent<RectTransform>().anchorMax.y >= 1
             && Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             Map.GetComponent<RectTransform>().anchorMin += new Vector2(-Input.GetAxis("Mouse ScrollWheel"), -Input.GetAxis("Mouse ScrollWheel"));
             Map.GetComponent<RectTransform>().anchorMax += new Vector2(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"));
-            Libertyville.GetComponent<RectTransform>().anchorMin = Map.GetComponent<RectTransform>().anchorMin;
-            Libertyville.GetComponent<RectTransform>().anchorMax = Map.GetComponent<RectTransform>().anchorMax;
+            //Libertyville.GetComponent<RectTransform>().anchorMin = Map.GetComponent<RectTransform>().anchorMin;
+            //Libertyville.GetComponent<RectTransform>().anchorMax = Map.GetComponent<RectTransform>().anchorMax;
         }
 
         else if (Map.GetComponent<RectTransform>().anchorMin.x >= -2.7 && Map.GetComponent<RectTransform>().anchorMin.y >= -2.7 &&
@@ -34,9 +35,18 @@ public class MAP : MonoBehaviour {
         {
             Map.GetComponent<RectTransform>().anchorMin += new Vector2(-Input.GetAxis("Mouse ScrollWheel"), -Input.GetAxis("Mouse ScrollWheel"));
             Map.GetComponent<RectTransform>().anchorMax += new Vector2(Input.GetAxis("Mouse ScrollWheel"), Input.GetAxis("Mouse ScrollWheel"));
-            Libertyville.GetComponent<RectTransform>().anchorMin = Map.GetComponent<RectTransform>().anchorMin;
-            Libertyville.GetComponent<RectTransform>().anchorMax = Map.GetComponent<RectTransform>().anchorMax
+            //Libertyville.GetComponent<RectTransform>().anchorMin = Map.GetComponent<RectTransform>().anchorMin;
+            //Libertyville.GetComponent<RectTransform>().anchorMax = Map.GetComponent<RectTransform>().anchorMax;
         }
+		*/
+		//Sane version
+		if(Input.GetAxis("Mouse ScrollWheel")>0){
+			Vector2 oldScale = Map.GetComponent<RectTransform>().localScale;
+			Map.GetComponent<RectTransform>().localScale = new Vector2(oldScale.x + 0.5f,oldScale.y + 0.5f);
+		} else if(Input.GetAxis("Mouse ScrollWheel")<0){
+			Vector2 oldScale = Map.GetComponent<RectTransform>().localScale;
+			Map.GetComponent<RectTransform>().localScale = new Vector2(oldScale.x - 0.5f,oldScale.y - 0.5f);
+		}
         
     }
 }
